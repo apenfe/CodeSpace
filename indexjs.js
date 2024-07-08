571c095f7d71a288ac8551f559e5c2ac63a0bf7c
apenfe <adrian1414@hotmail.es>
2024-07-08 11:51:23 +0200

    Primer commit

--- indexjs.js
+++ indexjs.js
@@ -1,2 +1,11 @@
 console.log("Hola mundo!");
 console.log("Hola mundo 2!");
+
+let nombre;
+nombre = "adrian";
+
+console.log("Hola " + nombre)
+
+let edad = 25;
+
+console.log("Hola " + nombre + " tienes "+edad+" años")
