698d227a6553dc9f2c6a753ca7e581d144ad4a6e
apenfe <adrian1414@hotmail.es>
2024-07-09 11:21:24 +0200

    operadores en js

---
+++ dia2.js
@@ -0,0 +1,14 @@
+let num1 = 2;
+let num2 = 5;
+
+let suma = num1 + num2
+console.log(suma)
+
+let resta = num1 - num2
+console.log(resta)
+
+let multiplicacion = num1 * num2
+console.log(multiplicacion)
+
+let division = num1 / num2
+console.log(division)
